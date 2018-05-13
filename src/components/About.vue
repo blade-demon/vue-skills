58f254117fb5f5c9285a39316a8d672a798c75e8
blade-demon <949659359@qq.com>
2018-05-13 10:17:18 +0800

    complete the project

---
+++ src/components/About.vue
@@ -0,0 +1,26 @@
+
+<template>
+<div>
+<h1>Hello {{ their_name }}, this is an about page</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ut optio officiis, dolorum aspernatur reiciendis accusantium itaque, nisi pariatur quisquam, obcaecati eaque placeat nesciunt? Rerum, cupiditate. Qui, explicabo suscipit. Tenetur?
+  </p>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/Skills.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min: 5'" name="skill">
+
+        <transition enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="error" v-if="errors.has('skill')"> {{errors.first('skill')}} </p>
+        </transition>
+      </form>
+      <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+        </li>
+        </transition-group>
+      </ul>
+        <p>These are the skills that you possess.</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skill: "",
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend Developer" }]
+    };
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.skills.push({ skill: this.skill });
+          this.skill = "";
+        } else {
+          console.log("no valid!");
+        }
+      });
+    },
+    remove(id) {
+      this.skills.splice(id, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./skills.css" scoped>
+
+</style>
